9ecbb031286c114117e9420c7fe4f2dbcb1d98ca
Adrien-Ray <forpub@accesdenied.net>
2021-06-11 09:29:35 +0200

    test preg

--- adrien/test-api/index.php
+++ adrien/test-api/index.php
@@ -1,4 +1,6 @@
 <?php
 require_once 'simple_dom/simple_html_dom.php';
 $dom = file_get_html('https://www.sportmag.fr/sport-handi', false);
-echo $dom;
+/* echo $dom; */
+$pattern = "/\" target=\"_self\">/i";
+echo preg_match_all($pattern, $dom);
