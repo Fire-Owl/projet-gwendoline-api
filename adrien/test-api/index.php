7517e052f2949f7c6fd66ff58ec37ad214d6d44e
Adrien-Ray <forpub@accesdenied.net>
2021-06-11 10:00:28 +0200

    a

--- adrien/test-api/index.php
+++ adrien/test-api/index.php
@@ -2,5 +2,9 @@
 require_once 'simple_dom/simple_html_dom.php';
 $dom = file_get_html('https://www.sportmag.fr/sport-handi', false);
 /* echo $dom; */
-$pattern = "/\" target=\"_self\">/i";
-echo preg_match_all($pattern, $dom);
+$pattern_last = "/<div class=\"edgtf-post-example-item-three-item edgtf-post-item\">/i";
+$pattern_previous = "/<div class=\"edgtf-peis-content-holder\">/i";
+$count_last = preg_match_all($pattern_last, $dom);
+$count_previous = preg_match_all($pattern_previous, $dom);
+$count_page = $count_last+$count_previous;
+echo 'nombre d\'articles présents sur cette page : '.$count_page;
