9dfe3decba4599f3ef53bf3fd8a912d3aa24d2a0
Gaël Sudour-Jeannin <67095523+Sastaras@users.noreply.github.com>
2021-06-11 11:24:28 +0200

    extractor

---
+++ tests/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a6b38052a6e9b14e70655f2393b1f6eedb8e07ba',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a6b38052a6e9b14e70655f2393b1f6eedb8e07ba',
+            'dev_requirement' => false,
+        ),
+        'fizzka/extractor' => array(
+            'pretty_version' => '0.3.5',
+            'version' => '0.3.5.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fizzka/extractor',
+            'aliases' => array(),
+            'reference' => '572cd2f3b65f050d54335ad0061b429246845e40',
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ tests/vendor/fizzka/extractor/src/Extractor.php
@@ -0,0 +1,76 @@
+<?php
+
+class Extractor extends SimpleXMLElement
+{
+    protected $isTopLevelElement = false;
+
+    public static function fromHtml($htmlString)
+    {
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $dom->preserveWhiteSpace = false;
+
+        if (!strlen($htmlString)) {
+            $htmlString = '<html></html>';
+        }
+
+        libxml_use_internal_errors(true);
+        $dom->loadHTML($htmlString);
+        libxml_clear_errors();
+
+        $ex = static::fromDom($dom);
+        $ex->isTopLevelElement = true;
+        return $ex;
+    }
+
+    public static function fromDom($dom)
+    {
+        return simplexml_import_dom($dom, get_class());
+    }
+
+    public function cssPath($expression)
+    {
+        $prefix = $this->isTopLevelElement ? '' : '.';
+        return $this->xpath($prefix . XpathSubquery::get($expression));
+    }
+
+    public function cssPathFirst($expression)
+    {
+        $res = $this->cssPath($expression);
+        if (count($res)) {
+            return $res[0];
+        }
+
+        return false;
+    }
+
+    public function xpathFirst($xpathQuery)
+    {
+        $res = $this->xpath($xpathQuery);
+        if (count($res)) {
+            return $res[0];
+        }
+
+        return false;
+    }
+
+    /**
+     * @deprecated, for compatibility with nokogiri (@see)
+     */
+    public function get($expression)
+    {
+        return $this->cssPath($expression);
+    }
+
+    /**
+     * @deprecated, for compatibility with nokogiri (@see)
+     */
+    public function getElements($xpathQuery)
+    {
+        return $this->xpath($xpathQuery);
+    }
+
+    public function innerText()
+    {
+        return strip_tags($this->asXML());
+    }
+}
