8e85e8db2581bb488b303ff243cc312094058ca2
Adrien-Ray <a.raymond@codeur.online>
2021-07-07 11:09:00 +0200

    manque thumb sportmag

--- scrapper/scrap-sportmag-handi.js
+++ scrapper/scrap-sportmag-handi.js
@@ -26,19 +26,25 @@ app.get("/", (req, res) =>
       var $ = cheerio.load(body);
       $(".edgtf-post-example-item-three-item").each(function (index, element) {
             country[index] = {};
-            country[index]["thumbnail"] = $(element)
-                  .find("img")
-                  .attr("href");
             country[index]["titre"] = $(element)
-                  .find(".edgtf-peis-title")
+                  .find("div.edgtf-post-example-item-three-item > div > div > h3:nth-child(1) > a:nth-child(1)")
                   .text()
                   .trim();
-            country[index]["auteur"] = $(element)
-                  .find(".edgtf-post-info-author-link")
-                  .attr("href");
             country[index]["date"] = $(element)
                   .find(".edgtf-post-info-date a span")
                   .text();
+            country[index]["accroche"] = $(element)
+                  .find("div.edgtf-post-example-item-three-item > div:nth-child(2) > div:nth-child(2) > p:nth-child(1)")
+                  .text();
+            /* country[index]["auteur"] = $(element)
+                  .find(".edgtf-post-info-author-link")
+                  .attr("href"); */
+            country[index]["lien"] = $(element)
+                  .find("div.edgtf-post-example-item-three-item > div:nth-child(1) > a:nth-child(3)")
+                  .attr("href");
+                  country[index]["thumbnail"] = $(element)
+                        .find("div.edgtf-post-example-item-three-item > div:nth-child(1) > div:nth-child(2) > img:nth-child(1)")
+                        .attr("src");
       });
 
       res.json(country);
