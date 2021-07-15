d9c9b6a41e8d9d76edbf8b1ddb6b3b438bf961f6
Adrien-Ray <forpub@accesdenied.net>
2021-07-15 13:44:21 +0200

    verif pour biblio

--- scrapper/scrap.js
+++ scrapper/scrap.js
@@ -1,182 +1,188 @@
 const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
+const cron = require('node-cron');
 const app = express();
 
 
+cron.schedule('*/0,10,20,30,40,50 * * * *', () => {
+    console.log('cron refresh');
+
 let promises = [];
+    
+    app.get('/', function (req, res) {
 
+        // SPORTMAG.FR
 
-app.get('/', function (req, res) {
+        promises.push(
+            new Promise((resolve, reject) => {
+                request(
+                    "https://www.sportmag.fr/sport-handi",
+                    function (error, response, body) {
+                        if (error) {
+                            reject.send(response.statusCode);
+                        }
+                        let article = [];
+                        let $ = cheerio.load(body);
+                        $(".edgtf-post-example-item-three-item").each(function (index, element) {
+                            article[index] = {};
+                            article[index]["titre"] = $(element)
+                                .find("div.edgtf-post-example-item-three-item > div > div > h3:nth-child(1) > a:nth-child(1)")
+                                .text()
+                                .trim();
+                            article[index]["date"] = $(element)
+                                .find(".edgtf-post-info-date a span")
+                                .text();
+                            article[index]["accroche"] = $(element)
+                                .find("div.edgtf-post-example-item-three-item > div:nth-child(2) > div:nth-child(2) > p:nth-child(1)")
+                                .text();
+                            /* country[index]["auteur"] = $(element)
+                                .find(".edgtf-post-info-author-link")
+                                .attr("href"); */
+                            article[index]["lien"] = $(element)
+                                .find("div.edgtf-post-example-item-three-item > div:nth-child(1) > a:nth-child(3)")
+                                .attr("href");
+                            article[index]["thumbnail"] = $(element)
+                                .find("div:nth-child(1) > div:nth-child(2) img")
+                                .attr("data-lazy-src");
+                        });
 
-    // SPORTMAG.FR
-
-    promises.push(
-        new Promise((resolve, reject) => {
-            request(
-                "https://www.sportmag.fr/sport-handi",
-                function (error, response, body) {
-                    if (error) {
-                        reject.send(response.statusCode);
+                        return resolve(article);
                     }
-                    let article = [];
-                    let $ = cheerio.load(body);
-                    $(".edgtf-post-example-item-three-item").each(function (index, element) {
-                        article[index] = {};
-                        article[index]["titre"] = $(element)
-                            .find("div.edgtf-post-example-item-three-item > div > div > h3:nth-child(1) > a:nth-child(1)")
-                            .text()
-                            .trim();
-                        article[index]["date"] = $(element)
-                            .find(".edgtf-post-info-date a span")
-                            .text();
-                        article[index]["accroche"] = $(element)
-                            .find("div.edgtf-post-example-item-three-item > div:nth-child(2) > div:nth-child(2) > p:nth-child(1)")
-                            .text();
-                        /* country[index]["auteur"] = $(element)
-                              .find(".edgtf-post-info-author-link")
-                              .attr("href"); */
-                        article[index]["lien"] = $(element)
-                            .find("div.edgtf-post-example-item-three-item > div:nth-child(1) > a:nth-child(3)")
-                            .attr("href");
-                        article[index]["thumbnail"] = $(element)
-                            .find("div:nth-child(1) > div:nth-child(2) img")
-                            .attr("data-lazy-src");
-                    });
+                );
+            })
+        );
 
-                    return resolve(article);
-                }
-            );
-        })
-    );
+        /* SITE EN ANGLAIS - PAS PERTINENT
+        // PARALYMPIC.ORG
 
-    /* SITE EN ANGLAIS - PAS PERTINENT
-    // PARALYMPIC.ORG
+        promises.push(
+            new Promise((resolve, reject) => {
+                request(
+                    "https://www.paralympic.org/goalball/news",
+                    function (error, response, body) {
+                        if (error) {
+                            reject.send(response.statusCode);
+                        }
+                        let article = [];
+                        let $ = cheerio.load(body);
+                        $("div.view-content > div > div").each(function (index, element) {
+                            article[index] = {};
+                            article[index]["link"] = $(element)
+                                .find("article > div > a")
+                                .attr("href");
+                        });
 
-    promises.push(
-        new Promise((resolve, reject) => {
-            request(
-                "https://www.paralympic.org/goalball/news",
-                function (error, response, body) {
-                    if (error) {
-                        reject.send(response.statusCode);
+                        return resolve(article);
                     }
-                    let article = [];
-                    let $ = cheerio.load(body);
-                    $("div.view-content > div > div").each(function (index, element) {
-                        article[index] = {};
-                        article[index]["link"] = $(element)
-                            .find("article > div > a")
-                            .attr("href");
-                    });
+                );
+            })
+        );
+        */
 
-                    return resolve(article);
-                }
-            );
-        })
-    );
-    */
+        // HANDISPORT/GOALBALL
 
-    // HANDISPORT/GOALBALL
+        promises.push(
+            new Promise((resolve, reject) => {
+                request(
+                    "http://www.handisport.org/?s=goalball",
+                    function (error, response, body) {
+                        if (error) {
+                            reject.send(response.statusCode);
+                        }
+                        let article = [];
+                        let $ = cheerio.load(body);
+                        $(".col-md-4").slice(0, 4).each(function (index, element) { //slice permet ici de récupérer seulement un certain nombre d'articles
+                            article[index] = {};
+                            article[index]["titre"] = $(element)
+                                .find("h2")
+                                .text()
+                                .trim();
 
-    promises.push(
-        new Promise((resolve, reject) => {
-            request(
-                "http://www.handisport.org/?s=goalball",
-                function (error, response, body) {
-                    if (error) {
-                        reject.send(response.statusCode);
+                            article[index]["lien"] = $(element)
+                                .find("a")
+                                .attr("href");
+
+                            article[index]["thumbnail"] = $(element)
+                                .find("a > div")
+                                .attr("style").replace('background:url(\'', '').replace('\') no-repeat', '');
+
+                            article[index]["date"] = $(element)
+                                .find(".date-actu")
+                                .text()
+                                .trim();
+
+                            article[index]["description"] = $(element)
+                                .find("div.arch")
+                                .text().replace(/\s\s+/g, '')
+                                .trim();
+
+                        });
+
+                        return resolve(article);
                     }
-                    let article = [];
-                    let $ = cheerio.load(body);
-                    $(".col-md-4").slice(0, 4).each(function (index, element) { //slice permet ici de récupérer seulement un certain nombre d'articles
-                        article[index] = {};
-                        article[index]["titre"] = $(element)
-                            .find("h2")
-                            .text()
-                            .trim();
+                );
+            })
+        );
 
-                        article[index]["lien"] = $(element)
-                            .find("a")
-                            .attr("href");
+        // HANDISPORT/EXPERTISE
 
-                        article[index]["thumbnail"] = $(element)
-                            .find("a > div")
-                            .attr("style").replace('background:url(\'', '').replace('\') no-repeat', '');
+        promises.push(
+            new Promise((resolve, reject) => {
+                request(
+                    "https://www.handisport.org/category/expertise/",
+                    function (error, response, body) {
+                        if (error) {
+                            reject.send(response.statusCode);
+                        }
+                        let article = [];
+                        let $ = cheerio.load(body);
+                        $(".col-md-4").slice(0, 4).each(function (index, element) {
+                            article[index] = {};
+                            article[index]["titre"] = $(element)
+                                .find("h2")
+                                .text()
+                                .trim();
 
-                        article[index]["date"] = $(element)
-                            .find(".date-actu")
-                            .text()
-                            .trim();
+                            article[index]["lien"] = $(element)
+                                .find("a")
+                                .attr("href");
 
-                        article[index]["description"] = $(element)
-                            .find("div.arch")
-                            .text().replace(/\s\s+/g, '')
-                            .trim();
+                            article[index]["thumbnail"] = $(element)
+                                .find("a > div")
+                                .attr("style").replace('background:url(\'', '').replace('\') no-repeat', '');
 
-                    });
+                            article[index]["date"] = $(element)
+                                .find(".date-actu")
+                                .text()
+                                .trim();
 
-                    return resolve(article);
-                }
-            );
-        })
-    );
+                            article[index]["description"] = $(element)
+                                .find("div.arch")
+                                .text().replace(/\s\s+/g, '')
+                                .trim();
 
-    // HANDISPORT/EXPERTISE
+                        });
 
-    promises.push(
-        new Promise((resolve, reject) => {
-            request(
-                "https://www.handisport.org/category/expertise/",
-                function (error, response, body) {
-                    if (error) {
-                        reject.send(response.statusCode);
+                        return resolve(article);
                     }
-                    let article = [];
-                    let $ = cheerio.load(body);
-                    $(".col-md-4").slice(0, 4).each(function (index, element) {
-                        article[index] = {};
-                        article[index]["titre"] = $(element)
-                            .find("h2")
-                            .text()
-                            .trim();
+                );
+            })
+        );
 
-                        article[index]["lien"] = $(element)
-                            .find("a")
-                            .attr("href");
-
-                        article[index]["thumbnail"] = $(element)
-                            .find("a > div")
-                            .attr("style").replace('background:url(\'', '').replace('\') no-repeat', '');
-
-                        article[index]["date"] = $(element)
-                            .find(".date-actu")
-                            .text()
-                            .trim();
-
-                        article[index]["description"] = $(element)
-                            .find("div.arch")
-                            .text().replace(/\s\s+/g, '')
-                            .trim();
-
-                    });
-
-                    return resolve(article);
-                }
-            );
-        })
-    );
-
-    // REPONSE
+        // REPONSE
 
 
-    Promise.all(promises)
-        .then((results) => {
-            res.json(results);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+        Promise.all(promises)
+            .then((results) => {
+                res.json(results);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+
+    });
 
 
 });
