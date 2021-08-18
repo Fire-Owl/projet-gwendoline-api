cd7bb9495ba7e43bfa9169d964b12f01c9340ac2
Adrien-Ray <a.raymond@codeur.online>
2021-08-18 15:02:13 +0200

    API terminer emballerCEstPese

--- test_client_boucle.js
+++ test_client_boucle.js
@@ -6,7 +6,7 @@ function boucleJson(json) {
       for (let i = 0; i < json.length; i++) {
             for (let ii = 0; ii < json[i].length; ii++) {
                   let inApiDiv = divApi.innerHTML;
-                  divApi.innerHTML = inApiDiv + '<a href="' + json[i][ii]['lien'] + '"><section class="apiArt"><img src="' + json[i][ii]['thumbnail'] + '"><h4>' + json[i][ii]['titre'] + '</h4>' + json[i][ii]['date'] + '<hr><p>' + json[i][ii]['description'] + '</p></section></a>';
+                  divApi.innerHTML = inApiDiv + '<a class="apiArt" href="' + json[i][ii]['lien'] + '"><section class="apiArt"><div class="child"><img src="' + json[i][ii]['thumbnail'] + '"></div><div class="child"><h4>' + json[i][ii]['titre'] + '</h4>' + json[i][ii]['date'] + '<hr><p>' + json[i][ii]['description'] + '</p></div></section></a>';
             }
       }
 }
