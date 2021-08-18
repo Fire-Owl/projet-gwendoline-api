b365831b947a77f8315624eec7d1e0a9ec02c5d2
Adrien-Ray <forpub@accesdenied.net>
2021-08-18 14:21:21 +0200

    lalala ça marche de ouf!

--- test_client_boucle.js
+++ test_client_boucle.js
@@ -6,7 +6,7 @@ function boucleJson(json) {
       for (let i = 0; i < json.length; i++) {
             for (let ii = 0; ii < json[i].length; ii++) {
                   let inApiDiv = divApi.innerHTML;
-                  divApi.innerHTML = inApiDiv + json[i][ii]['titre'] + '<br>';
+                  divApi.innerHTML = inApiDiv + '<a href="' + json[i][ii]['lien'] + '"><section class="apiArt"><img src="' + json[i][ii]['thumbnail'] + '"><h4>' + json[i][ii]['titre'] + '</h4>' + json[i][ii]['date'] + '<hr><p>' + json[i][ii]['description'] + '</p></section></a>';
             }
       }
 }
