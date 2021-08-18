1026dffd21ffe874c6651a922bdbf82a9edb98c1
Adrien-Ray <a.raymond@codeur.online>
2021-08-18 14:05:53 +0200

    for in for opérationnel

--- test_client_boucle.js
+++ test_client_boucle.js
@@ -1,13 +1,12 @@
 const divApi = document.getElementById('api');
-let inApiDiv = divApi.innerHTML;
-
-console.log('inApiDiv = ' + inApiDiv);
 
 function boucleJson(json) {
       /* divApi.innerHTML = json[0][0]['titre']; */
       /* console.log(json.length); */
       for (let i = 0; i < json.length; i++) {
-            console.log(i);
-            divApi.innerHTML = inApiDiv + json[i][0]['titre'];
+            for (let ii = 0; ii < json[i].length; ii++) {
+                  let inApiDiv = divApi.innerHTML;
+                  divApi.innerHTML = inApiDiv + json[i][ii]['titre'] + '<br>';
+            }
       }
 }
