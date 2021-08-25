84ff140b3af97820b49ecd647de89e9e21165429
“Dorce <dorce.raphael@gmail.com>
2021-08-25 14:37:43 +0200

    systeme de param dans l'url pour choisir sport

--- index.js
+++ index.js
@@ -4,177 +4,107 @@ const request = require("request");
 const cheerio = require("cheerio");
 const cron = require("node-cron");
 
-// pour faire passer des paramètres dans l'URL (pas sûr)
-// const bodyParser = require("body-parser");
-// app.use(bodyParser.json());
-
-var promises = [];
+let promises = [];
 
 // TEST CRON toutes les 6 heures
 // cron.schedule("* * */6 * * *", () => {
-promises = [];
+// promises = [];
 
-/*------------------------------------------- SPORTMAG.FR */
-promises.push(
-  new Promise((resolve, reject) => {
-    request(
-      "https://www.sportmag.fr/sport-handi",
-      function (error, response, body) {
-        if (error) {
-          reject.send(response.statusCode);
+function scrap(sport) {
+  promises = [];
+
+  /*------------------------------------------- SPORTMAG.FR */
+  promises.push(
+    new Promise((resolve, reject) => {
+      request(
+        `https://www.sportmag.fr/?s=handisport+${sport}`,
+        function (error, response, body) {
+          if (error) {
+            reject.send(response.statusCode);
+          }
+          let article = [];
+
+          try {
+            const $ = cheerio.load(body);
+            $(".jeg_pl_md_2").each(function (index, element) {
+              article[index] = {};
+              article[index]["lien"] = $(element).find("h3 a").attr("href");
+              article[index]["titre"] = $(element).find("h3 a").text().trim();
+              article[index]["thumbnail"] = $(element)
+                .find(".wp-post-image")
+                .attr("data-src");
+              article[index]["date"] = $(element)
+                .find(".jeg_meta_date a")
+                .text()
+                .trim();
+              article[index]["description"] = $(element)
+                .find(".jeg_post_excerpt p")
+                .text();
+            });
+            return resolve(article);
+          } catch (e) {
+            return resolve([]);
+          }
         }
-        var articleMain = [];
-        var article = [];
-        var articleTotal = [];
-        var $ = cheerio.load(body);
+      );
+    })
+  );
 
-        $(".edgtf-post-example-item-three-item").each(function (
-          index,
-          element
-        ) {
-          articleMain[index] = {};
-          articleMain[index]["mainlink"] = $(element)
-            .find(".edgtf-post-example-item-three-slide-link")
-            .attr("href");
-          articleMain[index]["maintitle"] = $(element)
-            .find(".edgtf-post-example-item-three-title")
-            .text()
-            .trim();
-          articleMain[index]["mainthumbnail"] = $(element)
-            .find("div.edgtf-post-example-item-three-image-inner-holder > img")
-            .attr("data-lazy-src");
-          articleMain[index]["maindate"] = $(element)
-            .find(".edgtf-post-info-date")
-            .text()
-            .trim();
-          articleMain[index]["maindescr"] = $(element)
-            .find(".edgtf-post-excerpt")
-            .text();
-        });
-        articleTotal.push(articleMain);
+  /*------------------------------------------- HANDISPORT */
+  promises.push(
+    new Promise((resolve, reject) => {
+      request(
+        `https://www.handisport.org/?s=${sport}`,
+        function (error, response, body) {
+          if (error) {
+            reject.send(response.statusCode);
+          }
+          let article = [];
 
-        $(".edgtf-peis-image-holder").each(function (index, element) {
-          article[index] = {};
-          article[index]["link"] = $(element)
-            .find(".edgtf-peis-link")
-            .attr("href");
-          // SLICE POUR AVOIR LA RESOLUTION MAX
-          article[index]["thumbnail"] =
-            $(element)
-              .find(".attachment-magazinevibe_edge_search_page_image")
-              .attr("data-lazy-src")
-              .slice(0, -12) +
-            $(element)
-              .find(".attachment-magazinevibe_edge_search_page_image")
-              .attr("data-lazy-src")
-              .slice(-4);
-        });
+          try {
+            const $ = cheerio.load(body);
+            $(".col-md-8 .col-md-4").each(function (index, element) {
+              article[index] = {};
+              article[index]["lien"] = $(element).find("a").attr("href");
+              article[index]["titre"] = $(element).find("h2").text().trim();
+              article[index]["thumbnail"] =
+                $(element)
+                  .find("a > div")
+                  .attr("style")
+                  .replace("background:url('", "")
+                  .replace("') no-repeat", "")
+                  .slice(0, -12) +
+                $(element)
+                  .find("a > div")
+                  .attr("style")
+                  .replace("background:url('", "")
+                  .replace("') no-repeat", "")
+                  .slice(-4);
+              article[index]["date"] = $(element)
+                .find(".date-actu")
+                .text()
+                .trim();
+              article[index]["description"] = $(element)
+                .find("div.arch")
+                .text()
+                .replace(/\s\s+/g, "")
+                .trim();
+            });
 
-        $(".edgtf-peis-content-holder").each(function (index, element) {
-          article[index]["title"] = $(element)
-            .find(".edgtf-peis-title")
-            .text()
-            .trim();
-          article[index]["date"] = $(element)
-            .find(".edgtf-post-info-date a span")
-            .text();
-          article[index]["descr"] = $(element)
-            .find(".edgtf-post-excerpt")
-            .text();
-        });
-        articleTotal.push(article);
-
-        return resolve(articleTotal);
-      }
-    );
-  })
-);
-
-/*------------------------------------------- PARALYMPIC.ORG */
-promises.push(
-  new Promise((resolve, reject) => {
-    request(
-      "https://www.paralympic.org/goalball/news",
-      function (error, response, body) {
-        if (error) {
-          reject.send(response.statusCode);
+            return resolve(article);
+          } catch (e) {
+            return resolve([]);
+          }
         }
-        var article = [];
-        var $ = cheerio.load(body);
-        $("div.view-content > div > div > article").each(function (
-          index,
-          element
-        ) {
-          article[index] = {};
-          article[index]["link"] = $(element)
-            .find("article > div > a")
-            .attr("href");
-          article[index]["title"] = $(element).find("h2 > span > div").text();
-          article[index]["thumbnail"] =
-            "https://www.paralympic.org" +
-            $(element)
-              .find("article > div > div.field__item > img")
-              .attr("data-src");
-        });
-
-        return resolve(article);
-      }
-    );
-  })
-);
-
-/*------------------------------------------- HANDISPORT */
-promises.push(
-  new Promise((resolve, reject) => {
-    request(
-      "https://www.handisport.org/category/expertise/",
-      function (error, response, body) {
-        if (error) {
-          reject.send(response.statusCode);
-        }
-        let article = [];
-        let $ = cheerio.load(body);
-        $(".col-md-4")
-          .slice(0, 4)
-          .each(function (index, element) {
-            article[index] = {};
-            article[index]["link"] = $(element).find("a").attr("href");
-            article[index]["title"] = $(element).find("h2").text().trim();
-            article[index]["thumbnail"] =
-              $(element)
-                .find("a > div")
-                .attr("style")
-                .replace("background:url('", "")
-                .replace("') no-repeat", "")
-                .slice(0, -12) +
-              $(element)
-                .find("a > div")
-                .attr("style")
-                .replace("background:url('", "")
-                .replace("') no-repeat", "")
-                .slice(-4);
-
-            article[index]["date"] = $(element)
-              .find(".date-actu")
-              .text()
-              .trim();
-
-            article[index]["descr"] = $(element)
-              .find("div.arch")
-              .text()
-              .replace(/\s\s+/g, "")
-              .trim();
-          });
-
-        return resolve(article);
-      }
-    );
-  })
-);
+      );
+    })
+  );
+}
 // });
 
 /*------------------------------------------- REPONSE */
-app.get("/goalball_api", (req, res) => {
+app.get("/goalball_api/:sport", (req, res) => {
+  scrap(req.params.sport);
   Promise.all(promises)
     .then((results) => {
       res.json(results);
@@ -183,12 +113,22 @@ app.get("/goalball_api", (req, res) => {
       console.log(error);
     });
 });
-
-/*------------------------------------------- Test parametre dans l'url */
-// app.get("/goalball_api/:postId", (req, res) => {
-//   Promise.all(promises).then(function (results) {
-//     res.json(results)[0].filter((element) => element.date == "21 Mai 2021");
-//   });
+/*------------------------------------------- test filtre */
+// app.get("/goalball_api/:sport/:filtre", (req, res) => {
+//   scrap(req.params.sport);
+//   Promise.all(promises)
+//     .then((results) => {
+//       let filteredArray = [];
+//       for (i = 0; i < results.length; i++) {
+//         filteredArray.push(
+//           results[i].filter((element) => element.date == req.params.filtre)
+//         );
+//       }
+//       res.json(filteredArray);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
 // });
 
 app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
