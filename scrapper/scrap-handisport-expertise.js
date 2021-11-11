c8f9f97b4bf1353489087ba997152ce352f0c5d7
Adrien-Ray <forpub@accesdenied.net>
2021-11-11 17:36:59 +0100

    ??

--- scrapper/scrap-handisport-expertise.js
+++ scrapper/scrap-handisport-expertise.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const request = require('request');
 const cheerio = require('cheerio');
 
@@ -43,49 +42,3 @@ module.exports = function(results) {
         }
     );
 };
-=======
-const request = require('request');
-const cheerio = require('cheerio');
-
-module.exports = function(results) {
-      
-    request(
-        "https://www.handisport.org/category/expertise/",
-        function (error, response, body) {
-            if (error) {
-                  return console.error('fetch fail handisport-expertise: ', error);
-            }
-            let article = [];
-            let $ = cheerio.load(body);
-            $(".col-md-4").slice(0, 4).each(function (index, element) {
-                article[index] = {};
-                article[index]["titre"] = $(element)
-                    .find("h2")
-                    .text()
-                    .trim();
-
-                article[index]["lien"] = $(element)
-                    .find("a")
-                    .attr("href");
-
-                article[index]["thumbnail"] = $(element)
-                    .find("a > div")
-                    .attr("style").replace('background:url(\'', '').replace('\') no-repeat', '');
-
-                article[index]["date"] = $(element)
-                    .find(".date-actu")
-                    .text()
-                    .trim();
-
-                article[index]["description"] = $(element)
-                    .find("div.arch")
-                    .text().replace(/\s\s+/g, '')
-                    .trim();
-
-            });
-
-            results.push(article);
-        }
-    );
-};
->>>>>>> 0d2758083222a37d5fc191b1e0166770faca75f1
