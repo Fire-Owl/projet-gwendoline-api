3680532debe9da315c71adc58f03a95f9cf2eb26
Adrien-Ray <forpub@accesdenied.net>
2021-11-11 17:31:39 +0100

    ?

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-fetch('https://adrienr.promo-68.codeur.online/projet-gwendoline-api/scrapper/node')
+fetch('https://accesdenied.net/archives/api-gwendoline/node')
       .then((res) => {
             /* console.log('ok') */
             res.json().then(j => displayJson(j))
